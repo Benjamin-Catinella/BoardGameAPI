509a77dc62798d61841654cfcfe68ac8a68e5dcc
Benjamin <bennhyon@gmail.com>
2023-01-19 09:59:03 +0100

    Initialisation

---
+++ src/main/java/com/quack/boardgameapi/BoardGameApiApplication.java
@@ -0,0 +1,13 @@
+package com.quack.boardgameapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardGameApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardGameApiApplication.class, args);
+    }
+
+}
